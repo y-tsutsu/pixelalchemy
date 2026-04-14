918ee1a3844b02b53b8ae4580aac42ac9b04dcce
tsutsu <tsutsu0928@gmail.com>
2026-04-14 21:12:48 +0900

    エッジ検出のデモをコンピュートシェーダーで再実装

--- gl_renderer.py
+++ gl_renderer.py
@@ -25,6 +25,10 @@ class GLRenderer(mglw.WindowConfig):
 
         # 仮テクスチャ（あとでサイズ更新）
         self.texture = None
+        self.edge_texture = None
+
+        with open('shaders/edge.comp') as fcomp:
+            self.comp = self.ctx.compute_shader(fcomp.read())
 
         with open('shaders/vertex.vert') as fvert, open('shaders/fragment.frag') as ffrag:
             self.prog = self.ctx.program(vertex_shader=fvert.read(), fragment_shader=ffrag.read())
@@ -46,9 +50,21 @@ class GLRenderer(mglw.WindowConfig):
         # 初回 or サイズ変更時にテクスチャ作成
         if self.texture is None or self.texture.size != (w, h):
             self.texture = self.ctx.texture((w, h), 4)
+            self.edge_texture = self.ctx.texture((w, h), 4)
             self.texture.build_mipmaps()
 
         self.texture.write(frame.tobytes())
+
         self.texture.use(location=0)
+        self.comp['src_tex'] = 0
+        self.edge_texture.bind_to_image(1, read=False, write=True)
+        gx, gy = (w + 15) // 16, (h + 15) // 16
+        self.comp.run(gx, gy)
+
+        self.ctx.memory_barrier()
+
+        self.texture.use(location=0)
+        self.edge_texture.use(location=1)
         self.prog['tex'] = 0
+        self.prog['edge_tex'] = 1
         self.vao.render(moderngl.TRIANGLE_STRIP)
